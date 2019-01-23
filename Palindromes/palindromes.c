7f8ab8a73c721a947e455e08475edc22b9fec229
Taoudi <taoudi128@gmail.com>
2019-01-23 13:46:13 +0100

    Binary Search Complete

--- Palindromes/palindromes.c
+++ Palindromes/palindromes.c
@@ -8,12 +8,18 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <cstring>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
 using namespace std;
 
 #define WORDCOUNT 25143
 #define MAXWORKERS 10   /* maximum number of workers */
 #define TRUE 1
 #define FALSE 0
+
+int counter = 0;
 
 double read_timer() {
     static bool initialized = false;
@@ -32,29 +38,39 @@ double read_timer() {
 int palindromic(string word){
   int boolean = TRUE;
   for(int i = 0, j=word.length()-1; i<=j;i++,j--){
-    if(word.at(i)!=word.at(j)){
+    if(tolower(word.at(i))!=tolower(word.at(j))){
       boolean=FALSE;
       break;
     }
   }
   return boolean;
 }
-/* for finding two word palindromes */
-int palindromic(string first, string second){
-  int boolean = TRUE;
-  if(sizeof(first)!=sizeof(second))
-    return FALSE;
-  for(int i = 0, j=word.length()-1; i<=j;i++,j--){
-    if(word.at(i)!=word.at(j)){
-      boolean=FALSE;
-      break;
+
+int binarySearch(string word, string words[]){
+  int boolean = FALSE;
+  double low = 0;
+  double high = WORDCOUNT-1;
+  string currentWord;
+  int index;
+  while(low <= high){
+    index = floor((low+high)/2);
+    currentWord = words[index];
+    int compared = strcasecmp(word.c_str(), currentWord.c_str());
+    if(compared < 0){
+        high = index-1;
     }
+    else if(compared > 0){
+      low = index+1;
+    }
+    else{
+        boolean = TRUE;
+        break;
+      }
   }
   return boolean;
 }
 
 int main(int argc, char *argv[]){
-
   ifstream inFile;
   string words[WORDCOUNT];
   /* read command line args if any */
@@ -64,12 +80,26 @@ int main(int argc, char *argv[]){
 
   /* Loading file data onto array */
   inFile.open("words");
-    for (int i = 0; i < WORDCOUNT; i++)
+    for (int i = 0; i < WORDCOUNT-1; i++)
     {
       inFile >> words[i];
     }
   inFile.close();
 
-
+  for(int i = 0; i<WORDCOUNT-1;i++){
+    if(palindromic(words[i])==TRUE){
+      printf("ONE WORD PAL %s\n", words[i].c_str());
+      counter++;
+    }
+    else{
+      string copy = words[i];
+      reverse(copy.begin(),copy.end());
+        if(binarySearch(copy,words)==TRUE){
+          counter++;
+          printf("TWO WORD PAL %s = %s\n", words[i].c_str(), copy.c_str());
+        }
+      }
+    }
+  printf("Counter: %d\n", counter);
 
 }
