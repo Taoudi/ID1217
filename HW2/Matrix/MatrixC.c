016290e013e318783db32fcdf6efaaf042375337
Taoudi <taoudi128@gmail.com>
2019-02-12 14:39:47 +0100

    trying

---
+++ HW2/Matrix/MatrixC.c
@@ -0,0 +1,131 @@
+/* matrix summation using OpenMP
+
+gcc-8 -Wall -fopenmp matrixA.c
+
+
+   usage with gcc (version 4.2 or higher required):
+     gcc -O -fopenmp -o matrixSum-openmp matrixSum-openmp.c
+     ./matrixSum-openmp size numWorkers
+
+*/
+
+#include <omp.h>
+#include <limits.h>
+#include <stdio.h>
+double start_time, end_time;
+
+#include <stdio.h>
+#define MAXSIZE 10000  /* maximum matrix size */
+#define MAXWORKERS 8   /* maximum number of workers */
+
+struct workerMax{
+  int max;
+  int maxPos[2];
+};
+
+struct workerMin{
+  int min;
+  int minPos[2];
+};
+
+omp_lock_t maxlock;
+omp_lock_t minlock;
+
+int numWorkers;
+int size;
+int matrix[MAXSIZE][MAXSIZE];
+void *Worker(void *);
+
+struct workerMax mymax(int new, int x, int y, struct workerMax data){
+//  printf("%d\t vs \t %d\n" , new, data.max);
+
+  if(new>data.max){
+    data.max = new;
+    data.maxPos[0] = x;
+    data.maxPos[1] = y;
+  }
+  return data;
+}
+
+struct workerMin mymin(int new, int x, int y, struct workerMin data){
+  //printf("%d\t vs \t %d\n" , new, data.min);
+
+  if(new<data.min){
+    data.min = new;
+    data.minPos[0] = x;
+    data.minPos[1] = y;
+  }
+  return data;
+}
+
+
+/* read command line, initialize, and create threads */
+int main(int argc, char *argv[]) {
+  int i, j, total=0;
+  struct workerMin skim;
+  skim.min = INT_MAX;
+  skim.minPos[0] = 101;
+  skim.maxPos[1]= 101;
+  /* read command line args if any */
+  size = (argc > 1)? atoi(argv[1]) : MAXSIZE;
+  numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
+  if (size > MAXSIZE) size = MAXSIZE;
+  if (numWorkers > MAXWORKERS) numWorkers = MAXWORKERS;
+  struct workerMax dataMax;
+  struct workerMin dataMin; /* partial data for min and max values */
+
+
+  omp_set_num_threads(numWorkers);
+
+  /* initialize the matrix */
+  for (i = 0; i < size; i++) {
+     printf("[ ");
+	  for (j = 0; j < size; j++) {
+      matrix[i][j] = rand()%99;
+      	  printf(" %d", matrix[i][j]);
+	  }
+	  	  printf(" ]\n");
+  }
+  omp_init_lock(&maxlock);
+  omp_init_lock(&minlock);
+  start_time = omp_get_wtime();
+
+
+  #pragma omp declare reduction (customMax:struct workerMax:omp_out=mymax(omp_in.max,omp_in.maxPos[0],omp_in.maxPos[1], omp_out))
+  dataMax.max=INT_MIN;
+
+  #pragma omp declare reduction (customMin:struct workerMin:omp_out=mymin(omp_in.min,omp_in.minPos[0],omp_in.minPos[1], omp_out))
+  dataMax.min=INT_MAX;
+
+#pragma omp parallel for reduction (+:total) reduction (customMax:dataMax) reduction (customMin:dataMin) private(j)
+  for (i = 0; i < size; i++)
+    for (j = 0; j < size; j++){
+      dataMax = mymax(matrix[i][j],i,j,dataMax);
+
+      if(matrix[i][j]<dataMin.min){
+        dataMin.min = matrix[i][j];
+        dataMin.minPos[0] = i;
+        dataMin.minPos[1] = j;
+      }
+
+      total += matrix[i][j];
+    }
+
+// implicit barrier
+
+  end_time = omp_get_wtime();
+
+  #pragma omp master
+  {
+    //Performance Evaluation
+    //printf("%f\n", end_time - start_time);
+
+
+  // NORMAL PRINT
+    printf("minimum %d at (%d,%d)\n", dataMin.min,dataMin.minPos[1]+1,dataMin.minPos[0]+1);
+    printf("max is %d at (%d,%d)\n", dataMax.max,dataMax.maxPos[1]+1,dataMax.maxPos[0]+1);
+    printf("the total is %d\n", total);
+    printf("it took %g seconds\n", end_time - start_time);
+}
+
+}
--- HW2/Matrix/script.sh
+++ HW2/Matrix/script.sh
@@ -1,4 +1,4 @@
-gcc-8 -fopenmp matrixB.c -o mat
+gcc-8 -fopenmp matrixC.c -o mat
 g++ performanceAnalyser.c -o pa
 for((i=1;i<=8;i++))
 do
